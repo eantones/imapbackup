64e6e0ac2c27c112aea9a0ec93e07b50a191bfbb
Eric Antones <eantones@nuobit.com>
2016-10-07 12:49:49 +0200

    [FIX] make 3.4 compatible

--- src/imapbackup.py
+++ src/imapbackup.py
@@ -489,7 +489,7 @@ def restore(from_filename, to_host, to_account, to_password, to_ssl=True):
                          where m.mailbox_id=? and
                                m.message_id not in (%s)""" % ','.join('?'*len(dest_message_ids))
 
-            src_messages = src.c.execute(src_sql, (mailbox_id, *dest_message_ids))
+            src_messages = src.c.execute(src_sql, tuple([mailbox_id] + list(dest_message_ids)))
             for message_id, message_pickle_bz2 in src_messages:
                 message_pickle = bz2.decompress(message_pickle_bz2)
                 message = pickle.loads(message_pickle)
